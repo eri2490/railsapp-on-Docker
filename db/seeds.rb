efa9a766cb7f8f49f68ca310daee3d307b2a37e1
eri2490 <e.147xxx@gmail.com>
2020-03-03 13:11:16 +0900

    Add faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,34 @@
+# User
+
 User.create!(
   name: "Example User",
-  email: "rails@railsorg.com",
+  email: "rails@rails.com",
   password: "foobar",
   password_confirmation: "foobar"
 )
 
 User.create!(
   name: "Sample User",
-  email: "sample@rails.com",
+  email: "sample@rails.org",
   password: "foobar",
   password_confirmation: "foobar"
 )
+
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@rails.org"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password
+  )
+end
+
+
+# Post
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(word_count = 5)
+  users.each { |user| user.posts.create!(content: content) }
+end
